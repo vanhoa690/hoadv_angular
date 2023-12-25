84b6f97da7982a240cd32baea48e82ffb59c6f42
vanhoa690 <vanhoa690@gmail.com>
2023-12-25 16:23:16 +0700

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,9 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home/home.component').then((a) => a.HomeComponent),
   },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((a) => a.LoginComponent),
+  },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
