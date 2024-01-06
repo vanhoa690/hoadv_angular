12ed36a9e7960db6c1262bd5af74d6825a0749c0
vanhoa690 <vanhoa690@gmail.com>
2024-01-06 19:42:44 +0700

    add call api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core'; // inject
+import { HttpClient } from '@angular/common/http'; // HttpClient
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   // call api
+  apiUrl = 'https://fakestoreapi.com/products'; // khai bao apiUrl
+  http = inject(HttpClient); // inject bien http
   constructor() {}
+
+  getProductList() {
+    return this.http.get(this.apiUrl); //axios.get(apiUrl)
+  }
 }
